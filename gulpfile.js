b36768c2d2aa84f15bc2586a51eaa8e400c63a31
magklax <nadic2006@yandex.ru>
2018-10-24 12:10:23 +0800

    минифицировала js-файлы

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 gulp.task("clean", function () {
   return del("build");
@@ -22,8 +24,7 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**"
+    "source/img/**"
   ], {
     base: "source"
   })
@@ -45,13 +46,23 @@ gulp.task("css", function () {
 });
 
 gulp.task("sprite", function () {
-//  return gulp.src("source/img/icon-*.svg")
   return gulp.src("source/img/inline/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("compress", function (cb) {
+  pump([
+        gulp.src("source/js/*.js"),
+        uglify(),
+        rename({suffix: ".min"}),
+        gulp.dest("build/js")
+    ],
+    cb
+  );
 });
 
 gulp.task("html", function () {
@@ -96,5 +107,5 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "compress", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
