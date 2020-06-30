4d0253d2c81cfb5b267d5e487913e86ed5992c7c
Nadia <33031855+magklax@users.noreply.github.com>
2020-06-30 12:40:52 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ gulp.task("minify", () => {
 });
 
 gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png,jvg,svg}")
+  return gulp.src("source/img/**/*.{png, jpg, svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
